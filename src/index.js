f055bb0db0169154099af89f71e4d36d01bcc4d8
Minh Quy <sugiacupit@gmail.com>
2018-04-17 00:41:39 +0200

    Remove css-loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 module.exports = {
-  cssLoader: require.resolve('./css-loader/loader'),
   styleLoader: require.resolve('./style-loader/index'),
   MiniCssExtractPlugin: require('./mini-css-extract-plugin/cjs'),
   miniCssExtractLoader: require.resolve('./mini-css-extract-plugin/loader'),
